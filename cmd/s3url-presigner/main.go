bf0a2f6d7b770c7acad63fb3e9824a92a9703f2e
Alex Veprik <alex.veprik@dolby.com>
2019-12-03 10:00:14 +1100

    Use default AWS credentials

--- cmd/s3url-presigner/main.go
+++ cmd/s3url-presigner/main.go
@@ -7,22 +7,16 @@ import (
 
 	"github.com/benderillo/s3url-presigner/pkg/storage"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	flags "github.com/jessevdk/go-flags"
 	"github.com/rs/zerolog/log"
 )
 
 var opts struct {
-	AwsRegion       string `short:"r" long:"aws-region" description:"AWS region" env:"AWS_REGION" required:"true"`
-	AwsAccessID     string `short:"i" long:"aws-access-id" description:"AWS access ID" env:"AWS_ACCESS_KEY_ID" required:"true"`
-	AwsSecretKey    string `short:"s" long:"aws-secret-key" description:"AWS secret key" env:"AWS_SECRET_ACCESS_KEY" required:"true"`
-	AwsSessionToken string `short:"t" long:"aws-session-token" description:"AWS session token" env:"AWS_SESSION_TOKEN"`
-	Bucket          string `short:"b" long:"bucket" description:"S3 bucket" env:"S3_BUCKET" required:"true"`
-	Path            string `short:"p" long:"path" description:"S3 path" env:"S3_PATH" required:"true"`
-	Method          string `short:"m" long:"method" choice:"get" choice:"put" description:"HTTP method that needs to be presigned" default:"get"`
-	Expiry          int64  `short:"e" long:"expiry" description:"Expiration time for the url in seconds" default:"7200"`
+	Bucket string `short:"b" long:"bucket" description:"S3 bucket" env:"S3_BUCKET" required:"true"`
+	Path   string `short:"p" long:"path" description:"S3 path" env:"S3_PATH" required:"true"`
+	Method string `short:"m" long:"method" choice:"get" choice:"put" description:"HTTP method that needs to be presigned" default:"get"`
+	Expiry int64  `short:"e" long:"expiry" description:"Expiration time for the url in seconds" default:"7200"`
 }
 
 func main() {
@@ -33,13 +27,8 @@ func main() {
 	}
 
 	// Create AWS session
-	awsSession, err := session.NewSession(&aws.Config{
-		Region: aws.String(opts.AwsRegion),
-		Credentials: credentials.NewStaticCredentials(
-			opts.AwsAccessID,
-			opts.AwsSecretKey,
-			opts.AwsSessionToken,
-		),
+	awsSession, err := session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
 	})
 
 	if err != nil {
